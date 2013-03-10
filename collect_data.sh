f5abcee68bdb3f2551aa068f7b06076c1428245f
Adam Hoddinott <adam@messymind.net>
2013-03-10 23:35:33 +1100

    Adding cron script

---
+++ collect_data.sh
@@ -0,0 +1,8 @@
+source /home/adam/.virtualenvs/rescraper/bin/activate
+
+SCRIPT=$(readlink -f $0)
+SCRIPTPATH=$(dirname $SCRIPT)
+
+${SCRIPTPATH}/manage.py collect_data
+
+echo "Last ran on: " + $(date) >> ${SCRIPTPATH}/scheduled.txt
